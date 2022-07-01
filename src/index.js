26017c3068ef3e1eb75241f5be96ef2af913c71a
SashaDyts <lentrofan@gmail.com>
2022-07-02 01:42:48 +0300

    sum

--- src/index.js
+++ src/index.js
@@ -7,16 +7,28 @@ const refs = {
 
 async function lox() {
   const data = await imagesApiService.fetchImages();
+  const dataGenres = await imagesApiService.getGenres();
+  console.log(dataGenres);
+  append(data, dataGenres);
 
-  append(data);
-  console.log(data);
+  // console.log(ff);
+  // imagesApiService.genres.map(res => res.id);
 }
 
 lox();
 
-function makeGalleryCardsMarkup(data) {
+function makeGalleryCardsMarkup(data, dataGenres) {
   return data
     .map(data => {
+      console.log(dataGenres);
+      let genresArray = dataGenres
+        .filter(genre => data.genre_ids.includes(genre.id))
+        .map(genre => genre.name);
+      let date = new Date(data.release_date);
+      let date2 = new Date(data.first_air_date);
+      let year = date.getFullYear();
+      let year2 = date2.getFullYear();
+      let vote = data.vote_average.toFixed(1);
       return `<li class="gallery__item">
         <a href="" class="gallery__link"><div class="card">
           <img src="https://image.tmdb.org/t/p/w500${
@@ -24,7 +36,9 @@ function makeGalleryCardsMarkup(data) {
           }" alt="" class="card__img" width="398"/>
           <p class="card__title">${data.title || data.name}</p>
           <div class="card__info">
-            <p class="card__text">Drama, Comedy | 2019<div class="card__ratting"><p class="card__rattimg-text">8.0</p></div></p>
+            <p class="card__text">${genresArray.splice(-3).join(', ')} | ${
+        year || year2
+      }<div class="card__ratting"><p class="card__rattimg-text">${vote}</p></div></p>
           </div>
           
         </div></a>
@@ -33,9 +47,16 @@ function makeGalleryCardsMarkup(data) {
     .join('');
 }
 
-function append(data) {
+function append(data, dataGenres) {
   refs.galleryItem.insertAdjacentHTML(
     'beforeend',
-    makeGalleryCardsMarkup(data)
+    makeGalleryCardsMarkup(data, dataGenres)
   );
 }
+
+console.log(imagesApiService.getGenres());
+
+const d = [1, 2, 3, 4, 5];
+
+d.splice(3);
+console.log(d);
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,18 +2,45 @@ import axios from 'axios';
 export class ImagesApiService {
   constructor() {
     this.id = [];
+    this.currentPage = 1;
+    this.API_KEY = '8491f0c88fa7dc1f853ccc0c4b339dca';
+    this.genres = [
+      { id: 28, name: 'Action' },
+      { id: 12, name: 'Adventure' },
+      { id: 16, name: 'Animation' },
+      { id: 35, name: 'Comedy' },
+      { id: 80, name: 'Crime' },
+      { id: 99, name: 'Documentary' },
+      { id: 18, name: 'Drama' },
+      { id: 10751, name: 'Family' },
+      { id: 14, name: 'Fantasy' },
+      { id: 36, name: 'History' },
+      { id: 27, name: 'Horror' },
+      { id: 10402, name: 'Music' },
+      { id: 9648, name: 'Mystery' },
+      { id: 10749, name: 'Romance' },
+      { id: 878, name: 'Science Fiction' },
+      { id: 10770, name: 'TV Movie' },
+      { id: 53, name: 'Thriller' },
+      { id: 10752, name: 'War' },
+      { id: 37, name: 'Western' },
+    ];
   }
 
   async fetchImages() {
-    const URL = `https://api.themoviedb.org/3/trending/all/day?api_key=8491f0c88fa7dc1f853ccc0c4b339dca`;
+    const URL = `https://api.themoviedb.org/3/trending/all/day?api_key=${this.API_KEY}&page=${this.currentPage}`;
     const options = {};
 
     const response = await axios.get(URL);
+    console.log(response);
     return response.data.results;
   }
 
-  async getMovieGenres(array) {
-    const results = await this.fetchImages();
-    return results.map(res => res.id);
+  async getGenres() {
+    const URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${this.API_KEY}&language=en-US`;
+
+    const response = await axios.get(URL);
+
+    return response.data.genres;
   }
 }
